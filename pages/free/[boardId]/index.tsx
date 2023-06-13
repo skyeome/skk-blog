efe894c0422767105dad545f9d27d4e84c52ffd2
skyeome <tjru7230@naver.com>
2023-06-13 20:54:57 +0900

    #1.7 댓글 수정

--- pages/free/[boardId]/index.tsx
+++ pages/free/[boardId]/index.tsx
@@ -1,6 +1,6 @@
 import BoardDetail from "../../../src/components/units/board/detail/BoardDetail.container";
 import CommentWrite from "../../../src/components/units/boardComment/write/CommentWrite.index";
-import CommentList from "../../../src/components/units/boardComment/list/CommentList.container";
+import CommentList from "../../../src/components/units/boardComment/list/CommentList.index";
 
 export default function BoardDetailPage(): JSX.Element {
   return (
